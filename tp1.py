8ee581d01ce793d84de4b6ac80937f2706e1f1d7
Emilie <emilie.allart@etudiant.univ-lille1.fr>
2015-01-27 16:08:19 +0100

    redaction des fonctions

--- tp1.py
+++ tp1.py
@@ -1,16 +1,30 @@
 import numpy as np
 
-t = np.loadtxt("t.txt")
-y = np.loadtxt("p.txt")
+def chargement () :
+	t = np.loadtxt("t.txt")
+	y = np.loadtxt("p.txt")
+	o = np.ones((1,t.shape[0]))
+	x = np.vstack((t,o))
+	return x,y
 
-o = np.ones((1,t.shape[0]))
+def calculTheta (x,y) :
+	# teta = (XXt)**-1 * (XY)
+	tmp1 = np.asmatrix(np.dot(x,x.T))
+	tmp2 = np.dot(x,y)
+	theta = np.dot(tmp1.I, tmp2)
+	return theta
 
-x = np.vstack((t,o))
-# teta = (XXt)**-1 * (XY)
-tmp1 = np.asmatrix(np.dot(x,x.T))
-tmp2 = np.dot(x,y)
-teta = np.dot(tmp1.I, tmp2)
+def calculErreurQuadra (x,y,theta) :
+	N = x.shape[0]
+	cumul = 0
+	xT = x.T
+	for i in range(1,N) :
+		tmp = np.dot(xT[i],theta)
+		cumul = (y[i] - tmp)**2
+	cumul = cumul/N
+	return cumul
 
-teta
-
-	
+if __name__ == "__main__" :
+	x,y = chargement()
+	theta = calculTheta(x,y)
+	print calculErreurQuadra (x,y,theta)
